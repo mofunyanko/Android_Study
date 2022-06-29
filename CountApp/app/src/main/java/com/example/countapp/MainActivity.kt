31e344fd0eef725639381e2596ee6e4f7aaceeae
mofunyanko <emiho.iwasaki@gmail.com>
2022-06-29 10:51:07 +0900

    リポジトリの統合

---
+++ CountApp/app/src/main/java/com/example/countapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.countapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberText: TextView = findViewById(R.id.numberText)
+        val plusButton: Button = findViewById(R.id.plusButton)
+        val minusButton: Button = findViewById(R.id.minusButton)
+        val clearButton: Button = findViewById(R.id.clearButton)
+
+        var num: Int = 0
+
+        plusButton.setOnClickListener{
+            num += 1
+            /*if(num % 3 == 0 && num != 0){
+                numberText.setTextColor(Color.parseColor("#FF9900"))
+            }else {
+                numberText.setTextColor(Color.parseColor("#6633CC"))
+            }*/
+            color(num)
+            numberText.text = num.toString()
+        }
+
+        minusButton.setOnClickListener{
+            num -= 1
+            /*if(num % 3 == 0 && num != 0){
+                numberText.setTextColor(Color.parseColor("#FF9900"))
+            }else {
+                numberText.setTextColor(Color.parseColor("#6633CC"))
+            }*/
+            color(num)
+            numberText.text = num.toString()
+        }
+
+        clearButton.setOnClickListener{
+            num = 0
+            numberText.text = num.toString()
+        }
+    }
+    fun color(num: Int) {
+        val numberText: TextView = findViewById(R.id.numberText)
+        if(num % 3 == 0 && num != 0){
+            numberText.setTextColor(Color.parseColor("#FF9900"))
+        }else {
+            numberText.setTextColor(Color.parseColor("#6633CC"))
+        }
+    }
+}
